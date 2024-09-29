a62750d29354560d42a385ea088921ba987f0df1
vabhavcoder <vabhavbhateja@gmail.com>
2024-09-29 18:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+<script>
+// Get the video
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+</script>
+
 function appendToDisplay(value) {
     document.getElementById('display').value += value;
 }
@@ -19,21 +38,3 @@ function calculateResult() {
         display.value = 'Error';
     }
 }
-<script>
-// Get the video
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-  if (video.paused) {
-    video.play();
-    btn.innerHTML = "Pause";
-  } else {
-    video.pause();
-    btn.innerHTML = "Play";
-  }
-}
-</script>
